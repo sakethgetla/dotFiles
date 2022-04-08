e20c723618fa8dcc0e684619025125e64d727b01
sakethgetla <saketh.getla@protonmail.com>
2022-04-08 11:26:07 +1000

    added shortcuts, emacs, qutebrowser

--- windowManagers/qtile/config.py
+++ windowManagers/qtile/config.py
@@ -54,6 +54,10 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    Key([mod], "s", lazy.spawn("emacs"), desc="start emacs"),
+    Key([mod], "t", lazy.spawn("qutebrowser"), desc="start emacs"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
