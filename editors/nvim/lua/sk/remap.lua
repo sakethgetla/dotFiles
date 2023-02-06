ab2064efe4418bc7c5e596068d9a74410edacc88
sakethgetla <saketh.getla@protonmail.com>
2023-02-06 21:58:08 +1100

    switch windows

--- editors/nvim/lua/sk/remap.lua
+++ editors/nvim/lua/sk/remap.lua
@@ -1,3 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ww", vim.cmd.w)
+
+vim.keymap.set("n", "<leader>mf", vim.lsp.buf.format)
+
+
+vim.keymap.set("n", "<leader>wh", "<C-w><C-h>")
+vim.keymap.set("n", "<leader>wl", "<C-w><C-l>")
+vim.keymap.set("n", "<leader>wj", "<C-w><C-j>")
+vim.keymap.set("n", "<leader>wk", "<C-w><C-k>")
+vim.keymap.set("n", "<leader>wq", "<C-w><C-q>")
