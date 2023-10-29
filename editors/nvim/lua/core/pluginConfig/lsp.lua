32fb872b6f3a3222203524a539dc3525c6e88a7a
sakethgetla <saketh.getla@protonmail.com>
2023-10-29 20:22:32 +1100

    lsp keymap

--- editors/nvim/lua/core/pluginConfig/lsp.lua
+++ editors/nvim/lua/core/pluginConfig/lsp.lua
@@ -3,6 +3,52 @@
 require('mason').setup()
 require('mason-lspconfig').setup()
 
+-- [[ Configure LSP ]]
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  -- NOTE: Remember that lua is a real programming language, and as such it is possible
+  -- to define small helper and utility functions so you don't have to repeat yourself
+  -- many times.
+  --
+  -- In this case, we create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  local telescope = require('telescope.builtin')
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
+  nmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
+  nmap('gI', telescope.lsp_implementations, '[G]oto [I]mplementation')
+  nmap('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinition')
+  nmap('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -19,7 +65,6 @@ local servers = {
   -- rust_analyzer = {},
   yamlls = {},
   jsonls = {},
-  lua_ls = {},
   bashls = {},
   grammarly = {},
   dockerls = {},
