1908e892e2716b4eb391b94d1f8df497e8846d9a
sakethgetla <saketh.getla@protonmail.com>
2023-11-05 21:17:44 +1100

    add format command

--- editors/nvim/lua/core/pluginConfig/lsp.lua
+++ editors/nvim/lua/core/pluginConfig/lsp.lua
@@ -44,10 +44,16 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  -- -- Create a command `:Format` local to the LSP buffer
+  -- nmap('<leader>fm', function(_)
+  --   vim.lsp.buf.format()
+  -- end, '[W]orkspace [R]emove Folder')
+
+  vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, { buffer = bufnr, desc = desc })
+
+  -- vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  --   vim.lsp.buf.format()
+  -- end, { desc = 'Format current buffer with LSP' })
 end
 
 -- Enable the following language servers
@@ -71,7 +77,7 @@ local servers = {
   -- yaml_language_server = {},
   sqlls = {},
   -- tsserver = {},
-  html = { filetypes = { 'html'} },
+  html = { filetypes = { 'html' } },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -111,7 +117,7 @@ mason_lspconfig.setup_handlers {
 --   ensure_installed = { "lua_ls", "tsserver", "pyright", "jsonls", "dockerls", "html", "grammarly", "awk_ls", "sqls",
 --     "yamlls", "prismals", "bashls" },
 -- }
--- 
+--
 
 -- -- local lspConfig = require("lspconfig")
 
@@ -138,49 +144,49 @@ mason_lspconfig.setup_handlers {
 -- --   -- vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, opts)
 -- --   vim.keymap.set('n', '<space>fm', vim.lsp.buf.format, opts)
 -- -- end
--- 
--- 
--- 
--- 
+--
+--
+--
+--
 -- -- lspConfig.lua_ls.setup {
 -- --   -- on_attach = onAttach
 -- -- }
 -- -- lspConfig.tsserver.setup {}
--- 
+--
 -- -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 -- -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
--- 
+--
 -- -- Enable some language servers with the additional completion capabilities offered by coq_nvim
 -- local servers = { 'tsserver', "lua_ls" }
--- 
+--
 -- -- local coq = require('coq')
--- 
+--
 -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- for _, lsp in ipairs(servers) do
 --   lspConfig[lsp].setup({
--- 
+--
 --       capabilities = capabilities,
 --   })
--- 
+--
 --   -- lspConfig[lsp].setup(coq.lsp_ensure_capabilities({
 --   --     capabilities = capabilities,
 --   --   -- on_attach = my_custom_on_attach,
 --   -- }))
 -- end
--- 
+--
 -- -- local lsp_defaults = lspConfig.util.default_config
--- -- 
+-- --
 -- -- lsp_defaults.capabilities = vim.tbl_deep_extend(
 -- --   'force',
 -- --   lsp_defaults.capabilities,
 -- --   -- require('coq').lsp_ensure_capabilities({})
 -- --   -- require('coq').default_capabilities()
 -- -- )
--- 
--- 
+--
+--
 -- -- coq.Now()
--- 
+--
 -- -- Use LspAttach autocommand to only map the following keys
 -- -- after the language server attaches to the current buffer
 -- vim.api.nvim_create_autocmd('LspAttach', {
@@ -188,7 +194,7 @@ mason_lspconfig.setup_handlers {
 --   callback = function(ev)
 --     -- Enable completion triggered by <c-x><c-o>
 --     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
--- 
+--
 --     -- Buffer local mappings.
 --     -- See `:help vim.lsp.*` for documentation on any of the below functions
 --     local opts = { buffer = ev.buf }
@@ -197,14 +203,14 @@ mason_lspconfig.setup_handlers {
 --     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 --     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 --     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
--- 
+--
 --     -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
 --     -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
 --     -- vim.keymap.set('n', '<space>wl', function()
 --     --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 --     -- end, opts)
--- 
--- 
+--
+--
 --     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
 --     -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 --     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
